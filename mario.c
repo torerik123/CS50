2672684c406f860d2e8ee9e062db8d6d01a15b1b
torerik123 <torerik123@users.noreply.github.com>
2020-04-19 18:10:24 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -7,18 +7,25 @@ int main(void)
     int n; //creates integer named n
     do     //do the following n times:
     {
-     n = get_int("Height:\n"); //takes a number from the user and stores it in variable n
+        n = get_int("Height:\n"); //takes a number from the user and stores it in variable n
     }
-     while (n < 1 || n > 8); //if n is less than 1, run loop again.
-     for (int i = 0; i < n ; i++) // Height = vertical columns
-     {
-         for (int j = 0; j < n; j++)
-         {
-             if (i + j < n -1)
-                printf(" ");
+    while (n < 1 || n > 8); //if n is less than 1, run loop again.
+    for (int i = 0; i < n ; i++) // Height = vertical columns.
+    {
+        for (int j = 0; j < n; j++) // prints rows with n characters until j < n = false
+
+        {
+            if (i + j < n - 1) //prints hashes and spaces
+            {
+                printf(" "); 
+            }
+
             else
-                printf("#");
-         }
-         printf("\n");
-     }
+            {
+                printf("#");      
+            }
+        }
+        printf("\n"); //newline
+    }
+
 } 
