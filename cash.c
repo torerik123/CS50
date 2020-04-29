ebea1636ab42256d585b44b7bfa73603591f46bd
torerik123 <torerik123@users.noreply.github.com>
2020-04-29 23:08:01 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -5,53 +5,59 @@
 int main(void)
 {
 
-float dollars; // initializing variable dollars
+    float dollars; // initializing variable dollars
 
-do
-{
-    dollars = get_float("Change: \n"); //Get user input and save in variable "dollars" as a float
-}
-
-while(dollars <= 0); //loops until condition is false, e.g. dollars is more than zero
-
-int cents = round(dollars * 100); //- Multiply dollars by 100, Round cents to nearest penny
-
-int counter = 0;
-int remainder = cents;
-
-
-do
-{
-    if (remainder - 25 > -1) // Quarters
+    do
     {
-        remainder = remainder - 25;
-        counter++;
+        dollars = get_float("Change: \n"); //Get user input and save in variable "dollars" as a float
     }
 
-    else if (remainder - 10 > -1) // Dimes
+    while (dollars <= 0); //loops until condition is false, e.g. dollars is more than zero
+
+    int cents = round(dollars * 100); //- Multiply dollars by 100, Round cents to nearest penny
+
+    int counter = 0;
+    int remainder = cents;
+
+
+    do // Loops as long as remainder is more than zero. If the condition is true, it executes the code inside the block before starting the loop again. If a condition is true, it will not check the other if-statements.
     {
-        remainder = remainder - 10;
-        counter++;
+        if (remainder - 25 > -1) // Quarters
+        {
+            remainder = remainder - 25;
+            counter++;
+        }
+
+        else if (remainder - 10 > -1) // Dimes
+        {
+            remainder = remainder - 10;
+            counter++;
+        }
+
+        else if (remainder - 5 > -1) // Nickels
+        {
+            remainder = remainder - 5;
+            counter++;
+        }
+
+        else if (remainder - 1 > -1) // Pennies
+        {
+            remainder = remainder - 1;
+            counter++;
+        }
+
+        else
+        {
+            break;
+        }
+
     }
 
-    else if (remainder - 5 > -1) // Nickels
-    {
-        remainder = remainder - 5;
-        counter++;
-    }
 
-    else if (remainder - 1 > -1) // Pennies
-    {
-        remainder = remainder - 1;
-        counter++;
-    }
-
-    else
-    break;
-
-}
-while(remainder > 0 );
+    while (remainder > 0);
 
 
-printf("%i\n", counter);
+
+
+    printf("%i\n", counter);
 }
