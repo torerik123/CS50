4640273bf0ae7c1525cc93fe556822fef29dad63
torerik123 <torerik123@users.noreply.github.com>
2020-06-04 09:09:48 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -27,69 +27,69 @@ int main(int argc, char *argv[])
     else
     {
 
-    //  Look for beginning of JPEG
-    //  Buffer array to store block to read
-    unsigned char buffer[512];
+        //  Look for beginning of JPEG
+        //  Buffer array to store block to read
+        unsigned char buffer[512];
 
-    //  Count JPEGS
-    int jpgcounter = 0;
+        //  Count JPEGS
+        int jpgcounter = 0;
 
-    //  Make filename - Array to store 7 ints (000.jpg) + NULL character
-    char name[8];
+        //  Make filename - Array to store 7 ints (000.jpg) + NULL character
+        char name[8];
 
-    FILE* img = NULL;
+        FILE *img = NULL;
 
-    //  Loops until end of card - When fread can not read any more bytes the condition will be false
-    while (fread(buffer, 512, 1, file) == 1)
-    {
-        //  If start of new JPEG
-        if (buffer[0] == 0xff
-            && buffer[1] == 0xd8
-            && buffer[2] == 0xff
-            && (buffer[3] & 0xf0) == 0xe0)
+        //  Loops until end of card - When fread can not read any more bytes the condition will be false
+        while (fread(buffer, 512, 1, file) == 1)
         {
-            //  If first JPEG
-            if (jpgcounter == 0)
+            //  If start of new JPEG
+            if (buffer[0] == 0xff
+                && buffer[1] == 0xd8
+                && buffer[2] == 0xff
+                && (buffer[3] & 0xf0) == 0xe0)
             {
-                //  Print string to filename
-                sprintf(name,"%03i.jpg", jpgcounter);
+                //  If first JPEG
+                if (jpgcounter == 0)
+                {
+                    //  Print string to filename
+                    sprintf(name, "%03i.jpg", jpgcounter);
 
-                //  Open new JPEG file to write to
-                img = fopen(name, "w");
+                    //  Open new JPEG file to write to
+                    img = fopen(name, "w");
 
-                //  Write 512 bytes from buffer array into img
-                fwrite(buffer,512,1, img);
-                jpgcounter++;
+                    //  Write 512 bytes from buffer array into img
+                    fwrite(buffer, 512, 1, img);
+                    jpgcounter++;
+                }
+
+                else
+                {
+                    fclose(img);
+                
+                    //  Print string to filename
+                    sprintf(name, "%03i.jpg", jpgcounter);
+                    
+                    //  Open new JPEG file to write to
+                    img = fopen(name, "w");
+                
+                    //  Write 512 bytes from buffer array into img
+                    fwrite(buffer, 512, 1, img);
+                    jpgcounter++;
+                }
             }
 
+            //  If it is not the start of a JPEG, that means we have already started writing to a file
             else
             {
-                fclose(img);
-                
-                //  Print string to filename
-                sprintf(name,"%03i.jpg", jpgcounter);
-                //  Open new JPEG file to write to
-                img = fopen(name, "w");
-                
-                //  Write 512 bytes from buffer array into img
-                fwrite(buffer,512,1, img);
-                jpgcounter++;
+                if (jpgcounter > 0) // If # of JPEGS is more than 0, meaning it is not the first JPEG
+                {
+                    //  Write 512 bytes from buffer array into img
+                    fwrite(buffer, 512, 1, img);
+                }
             }
         }
-
-        //  If it is not the start of a JPEG, that means we have already started writing to a file
-        else
-        {
-            if (jpgcounter > 0) // If # of JPEGS is more than 0, meaning it is not the first JPEG
-            {
-                //  Write 512 bytes from buffer array into img
-                fwrite(buffer,512,1, img);
-            }
-        }
-    }
-    printf("JPEGS found: %i\n", jpgcounter);
-
-    //  Close remaining files
-    fclose(img);
+    
+        //  Close remaining files
+        fclose(img);
     }
 }
