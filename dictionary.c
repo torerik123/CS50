fc908f8fe43a4a0207d6cf73d39f2caafe8eeef7
torerik123 <torerik123@users.noreply.github.com>
2020-06-13 06:48:36 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -55,21 +55,12 @@ bool check(const char *word)
 
 unsigned int hash(const char *word)
 {
-    //  Make word lowercase before hashing
-    int x = strlen(word) + 1;
-    
-    char *lowercase = malloc(x * sizeof(char));
-
-    for (int i = 0; i < x; i++)
-    {
-        lowercase[i] = tolower(word[i]);
-    }
 
     //  Hash word
     unsigned int hash = 5381;
     int c = 0;
 
-    while ((c = *lowercase++))
+    while ((c = *word++))
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
     return hash % N;
