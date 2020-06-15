3c24bcdcef693550dd4cfb7810ff5f2ea888b9d9
torerik123 <torerik123@users.noreply.github.com>
2020-06-15 01:51:47 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -44,7 +44,7 @@ bool check(const char *word)
     //Keep moving until cursor == NULL
     while (cursor != NULL)
     {
-        if (strcasecmp(cursor->word, word) == 0)
+        if (strcasecmp(cursor->word, word) != 1)
         {
             return true;
         }
