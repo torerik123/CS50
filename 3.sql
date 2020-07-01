3781299dc566af3c4f7fccabb18ec83d07a92f5b
torerik123 <torerik123@users.noreply.github.com>
2020-07-01 08:19:58 +0000

    automated commit by check50 [check50=True]

--- 3.sql
+++ 3.sql
@@ -1,3 +1,3 @@
  /*list the titles of all movies with a release date on or after 2018, in alphabetical order.
 Movies released in 2018 should be included, as should movies with release dates in the future.*/
-SELECT title from movies WHERE year > 2018;
+SELECT title from movies WHERE year >= 2018 ORDER BY title;
