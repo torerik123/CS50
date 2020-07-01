ca9e04acba3cbca536561fd7d11f23176b5485cb
torerik123 <torerik123@users.noreply.github.com>
2020-07-01 08:31:22 +0000

    automated commit by check50 [check50=True]

--- 10.sql
+++ 10.sql
@@ -1,5 +1,5 @@
 /*List the names of all people who have directed a movie that received a rating of at least 9.0.
 Your query should output a table with a single column for the name of each person.*/
 
-SELECT COUNT(name) FROM
+SELECT name FROM
 people JOIN directors ON directors.person_id == people.id JOIN ratings ON ratings.movie_id == directors.movie_id WHERE rating >= 9.0;
