82b3730afae473ac4d1b4fda3ae2d48fa94aa628
torerik123 <torerik123@users.noreply.github.com>
2020-07-02 14:16:06 +0000

    automated commit by check50 [check50=True]

--- import.py
+++ import.py
@@ -2,71 +2,74 @@ from cs50 import SQL
 from sys import argv, exit
 import csv
 
-# Create empty students.db
-open("students.db", "w").close()
 
-# Open SQL Database for sqlite to save student info
-db = SQL("sqlite:///students.db")
+def main():
+    # Create empty students.db
+    open("students.db", "w").close()
 
-# Create table students in database students.db
-db.execute("CREATE TABLE students(first TEXT, middle TEXT, last TEXT, house TEXT, birth NUMERIC)")
+    # Open SQL Database for sqlite to save student info
+    db = SQL("sqlite:///students.db")
 
-# Check for command line arguments
-if len(argv) != 2:
-    print("Usage: python import.py file.csv")
-    exit(1)
+    # Create table students in database students.db
+    db.execute("CREATE TABLE students(first TEXT, middle TEXT, last TEXT, house TEXT, birth NUMERIC)")
 
-# If CSV file exists
-else:
+    # Check for command line arguments
+    if len(argv) != 2:
+        print("Usage: python import.py file.csv")
+        exit(1)
 
-    # Open file
-    csv_names = argv[1]
-    with open(csv_names, "r") as names:
-        reader = csv.reader(names)
+    # If CSV file exists
+    else:
 
-        #Skip first row
-        next(reader)
+        # Open file
+        csv_names = argv[1]
+        with open(csv_names, "r") as names:
+            reader = csv.reader(names)
 
-        # Dictionary to store student info
-        dict_houses = {"first:":"", "middle":"", "last:":"", "house":"", "birth":""}
+            #Skip first row
+            next(reader)
 
-        for row in reader:
+            # Dictionary to store student info
+            dict_houses = {"first:":"", "middle":"", "last:":"", "house":"", "birth":""}
 
-            # Names
-            if row[0]:
+            for row in reader:
 
-                # Splits string into list of names
-                s = row[0].split()
+                # Names
+                if row[0]:
 
-                # Joins the item in list to a string
-                first = ''.join(s[0:1])
-                last = ''.join(s[-1])
+                    # Splits string into list of names
+                    s = row[0].split()
 
-                if len(s) > 2:
+                    # Joins the item in list to a string
+                    first = ''.join(s[0:1])
+                    last = ''.join(s[-1])
 
-                    middle = ''.join(s[1:-1])
+                    if len(s) > 2:
 
-                else:
+                        middle = ''.join(s[1:-1])
 
-                    middle = None
+                    else:
 
-                # Add to dictionary
-                dict_houses.update({"first:": first, "middle": middle, "last:": last})
+                        middle = None
 
-            # houses
-            if row[1]:
+                    # Add to dictionary
+                    dict_houses.update({"first:": first, "middle": middle, "last:": last})
 
-                house = "".join(row[1])
-                dict_houses["house"] = house
+                # houses
+                if row[1]:
 
+                    house = "".join(row[1])
+                    dict_houses["house"] = house
 
-            # birth year
-            if row[2]:
+                # birth year
+                if row[2]:
 
-                birth = "".join(row[2])
-                dict_houses["birth"] = birth
+                    birth = "".join(row[2])
+                    dict_houses["birth"] = birth
 
-            # Write to database
-            db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?,?,?,?,?)",
-            first, middle, last, house, birth)
-            #print(dict_houses.values())
+                # Write to database
+                db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?,?,?,?,?)",
+                first, middle, last, house, birth)
+
+if __name__ == '__main__':
+    main()
