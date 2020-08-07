99626e340fdea4b705e22f47ec72b05b8ed202da
torerik123 <62653782+torerik123@users.noreply.github.com>
2020-08-08 01:19:45 +0200

    Add files via upload

---
+++ pset1/cash/cash.c
@@ -0,0 +1,63 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+
+    float dollars; // initializing variable dollars
+
+    do
+    {
+        dollars = get_float("Change: \n"); //Get user input and save in variable "dollars" as a float
+    }
+
+    while (dollars <= 0); //loops until condition is false, e.g. dollars is more than zero
+
+    int cents = round(dollars * 100); //- Multiply dollars by 100, Round cents to nearest penny
+
+    int counter = 0;
+    int remainder = cents;
+
+
+    do // Loops as long as remainder is more than zero. If the condition is true, it executes the code inside the block before starting the loop again. If a condition is true, it will not check the other if-statements.
+    {
+        if (remainder - 25 > -1) // Quarters
+        {
+            remainder = remainder - 25;
+            counter++;
+        }
+
+        else if (remainder - 10 > -1) // Dimes
+        {
+            remainder = remainder - 10;
+            counter++;
+        }
+
+        else if (remainder - 5 > -1) // Nickels
+        {
+            remainder = remainder - 5;
+            counter++;
+        }
+
+        else if (remainder - 1 > -1) // Pennies
+        {
+            remainder = remainder - 1;
+            counter++;
+        }
+
+        else
+        {
+            break;
+        }
+
+    }
+
+
+    while (remainder > 0);
+
+
+
+
+    printf("%i\n", counter);
+}
